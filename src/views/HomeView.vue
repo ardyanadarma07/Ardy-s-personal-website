3f87961ef4716ac8bfb0f894d201ad74e98b0f3a
Ardyana Darma <ardyanadarma@gmail.com>
2026-02-14 10:57:11 +0800

    nambahin tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="h-100 w-100 bg-gray-200 absolute z-0 flex flex-row">
+        <div class="h-120 w-20 bg-green-400">
+            <p>Anjay</p>
+        </div>
+        <div class="h-120 w-20 bg-blue-400">
+            <p>Wkwk</p>
+        </div>
+    </div>
 </template>
+
+<script>
+
+</script>
