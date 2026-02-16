7b6512af0f945eb760699d079009f443002dfdf0
Ardyana Darma <ardyanadarma@gmail.com>
2026-02-16 14:35:28 +0800

    nyicil beberapa komponen

---
+++ src/components/printilan/SlideButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex w-fit h-fit gap-4">
+    <!-- button kiri -->
+    <button
+      class="w-20 h-20 rounded-full bg-base border-2 border-gelap justify-center items-center flex hover:bg-gelap transision-all duration-500 group"
+    >
+      <svg
+        width="18"
+        height="28"
+        viewBox="0 0 18 28"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="text-gelap group-hover:text-base"
+      >
+        <path
+          d="M6.81081 14L18 24.7333L14.5946 28L0 14L14.5946 0L18 3.26667L6.81081 14Z"
+          fill="currentColor"
+        />
+      </svg>
+    </button>
+    <!-- button kanan -->
+    <button
+      class="w-20 h-20 rounded-full bg-base border-2 border-gelap justify-center items-center flex hover:bg-gelap transision-all duration-500 group"
+    >
+      <svg
+        width="18"
+        height="28"
+        viewBox="0 0 18 28"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="text-gelap group-hover:text-base"
+      >
+        <path
+          d="M11.1892 14L0 24.7333L3.40541 28L18 14L3.40541 0L0 3.26667L11.1892 14Z"
+          fill="currentColor"
+        />
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script setup>
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="w-full h-screen flex items-center justify-center">
-    <Sosmed class="group cursor-default">
-      
-    </Sosmed>
+    <SlideButton/>
   </div>
 </template>
 
 <script setup>
-import Navbar from '@/components/componen-gede/navbar.vue'
+import logoVue from '@/assets/VueJs.png'
+import Navbar from '@/components/componen-gede/NavBar.vue'
 import DownloadCV from '@/components/printilan/DownloadCV.vue'
 import GetInTouch from '@/components/printilan/GetInTouch.vue'
 import NavItem from '@/components/printilan/NavItem.vue'
 import Sosmed from '@/components/printilan/Sosmed.vue'
+import SkillCard from '@/components/printilan/SkillCard.vue'
+import SlideButton from '@/components/printilan/SlideButton.vue'
 </script>
